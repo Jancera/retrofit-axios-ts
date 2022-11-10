dbe59467feb697eafcaf2cf58bdf9922d0681894
Jancer Lima <contato.jancersl@gmail.com>
2022-11-10 17:16:51 -0300

    Cleaning TODOS

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
 export * from "./baseService";
 export * from "./decorators";
-
-// [-] Work on interceptors
-// [ ] Accept interceptors optionally
-// [ ] Work on abort controller
-// [ ] Work on file request progress
-// [ ] Change test file name from ts-retrofit to package name
